27448aec881a683fc697b9b49a8efb8cde1a3c3e
gamze <gamzekandara@gmail.com>
2024-02-14 19:17:37 -0500

    Adds QuestionList.js, GET request

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect , useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [ questions, setQuestions ] = useState([])
+  // this component need access to the question data
+  // make fetch request to display the questions 
+  // state => questions , setQuestions will hold the data
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((res) => res.json())
+    .then((questionsArr) => setQuestions(questionsArr))
+  }, [])
+
+  console.log(questions) // state updated
+
+  const lis = questions.map((obj) => { 
+    return <QuestionItem key={obj.id} question={obj}/>
+  })
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{lis}</ul>
     </section>
   );
 }
