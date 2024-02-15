da94ff204386f6dd1c9d5606602dc75fbab027fd
gamze <gamzekandara@gmail.com>
2024-02-14 20:23:57 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [ questions, setQuestions ] = useState([]);
+
+  // this component need access to the question data
+  // make fetch request to display the questions 
+  // state => questions , setQuestions will hold the data
+  // data flow => parent to child
+
+  //When child components update the state of their parent components in Effects, the data flow becomes very difficult to trace. Since both the child and the parent need the same data, let the parent component fetch that data, and pass it down to the child instead:
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((res) => res.json())
+    .then((questionsArr) => setQuestions(questionsArr))
+  }, [])
+
+  console.log(questions) // state updated
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm  setQuestions={setQuestions} /> : <QuestionList questions={questions} setQuestions={setQuestions} />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,19 +1,7 @@
-import React, { useEffect , useState } from "react";
+import React from "react";
 import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
-  const [ questions, setQuestions ] = useState([])
-  // this component need access to the question data
-  // make fetch request to display the questions 
-  // state => questions , setQuestions will hold the data
-
-  useEffect(() => {
-    fetch("http://localhost:4000/questions")
-    .then((res) => res.json())
-    .then((questionsArr) => setQuestions(questionsArr))
-  }, [])
-
-  console.log(questions) // state updated
+function QuestionList({ questions }) {  
 
   const lis = questions.map((obj) => { 
     return <QuestionItem key={obj.id} question={obj}/>
